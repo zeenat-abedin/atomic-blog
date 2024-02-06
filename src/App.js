ab18cc3ed163a4470245a9bc909d2230d4568203
zeenat-abedin <zeenu.23@gmail.com>
2024-02-06 20:08:12 +0530

    Added a fake dark mode toggle button and a large archive of fake posts to the app.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
+import { PostProvider } from "../contexts/PostContext";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
 
 function App() {
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
   return (
     <section>
-      {/* <button
+      <button
         onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
         className="btn-fake-dark-mode"
       >
         {isFakeDark ? "☀️" : "🌙"}
-      </button> */}
+      </button>
 
-      <Header />
-      <Main />
-      <Archive />
-      <Footer />
+      <PostProvider>
+        <Header />
+        <Main />
+        <Archive />
+        <Footer />
+      </PostProvider>
     </section>
   );
 }
@@ -112,10 +131,10 @@ function List({ posts }) {
 
 function Archive({ onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-  // const [posts] = useState(() =>
-  //   // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-  //   Array.from({ length: 10000 }, () => createRandomPost())
-  // );
+  const [posts] = useState(() =>
+    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+    Array.from({ length: 10000 }, () => createRandomPost())
+  );
 
   const [showArchive, setShowArchive] = useState(false);
 
@@ -126,7 +145,7 @@ function Archive({ onAddPost }) {
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
 
-      {/* {showArchive && (
+      {showArchive && (
         <ul>
           {posts.map((post, i) => (
             <li key={i}>
@@ -137,7 +156,7 @@ function Archive({ onAddPost }) {
             </li>
           ))}
         </ul>
-      )} */}
+      )}
     </aside>
   );
 }
